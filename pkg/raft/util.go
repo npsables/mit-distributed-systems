bacc31d45ee4f610915dcae4da6b2ab14ecfb4a8
Nguyen Phuong <phuongnguyen.ng3010@gmail.com>
2023-02-05 14:57:29 +0700

    better debug

--- pkg/raft/util.go
+++ pkg/raft/util.go
@@ -1,13 +1,13 @@
 package raft
 
-import "log"
-
-// Debugging
-const Debug = false
+import (
+	"fmt"
+	"os"
+)
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
-	if Debug {
-		log.Printf(format, a...)
+	if os.Getenv("DEBUG") == "1" {
+		fmt.Printf(format, a...)
 	}
 	return
 }
